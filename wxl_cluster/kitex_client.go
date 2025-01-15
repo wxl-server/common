ba42dfa3b6f401f5c4e01a00eaef5b18e0bdb162
Wang Xinglong <wangxinglong.123@bytedance.com>
2025-01-15 20:55:34 +0800

    update

--- wxl_cluster/kitex_client.go
+++ wxl_cluster/kitex_client.go
@@ -5,6 +5,7 @@ import (
 	"github.com/cloudwego/kitex/client"
 	"github.com/cloudwego/kitex/pkg/retry"
 	"github.com/kitex-contrib/registry-nacos/resolver"
+	"github.com/qcq1/common/gptr"
 	"github.com/qcq1/common/wxl_cluster/kitex_middleware"
 	"github.com/qcq1/common/wxl_cluster/nacos"
 )
@@ -13,32 +14,30 @@ const (
 	MaxRetryTimes = 3
 )
 
-type RawCallStruct[T any] struct {
-	client T
-	rp     *retry.FailurePolicy
+type RawCallInterface[T any] interface {
+	setClient(T)
+	setRetryPolicy(*retry.FailurePolicy)
 }
 
 type newClientFunc[T any] func(destService string, opts ...client.Option) (T, error)
 
-func NewClient[T any](newClient newClientFunc[T], destService string) *RawCallStruct[T] {
-	RawCall := &RawCallStruct[T]{}
-
+func NewClient[T any, M any](newClient newClientFunc[T], destService string, RawCall *RawCallInterface[T]) {
 	cli, err := nacos.NewNacosClient()
 	if err != nil {
 		logger.Errorf("[Init] clients.NewNamingClient failed, err = %v", err)
 		panic(err)
 	}
-	RawCall.client, err = newClient(
+	destClient, err := newClient(
 		destService,
 		client.WithResolver(resolver.NewNacosResolver(cli)),
 		client.WithMiddleware(kitex_middleware.LogMiddleware),
 	)
+	gptr.Indirect(RawCall).setClient(destClient)
 	if err != nil {
 		logger.Errorf("[Init] minercore.NewClient failed, err = %v", err)
 		panic(err)
 	}
 	rp := retry.NewFailurePolicy()
 	rp.WithMaxRetryTimes(MaxRetryTimes)
-	RawCall.rp = rp
-	return RawCall
+	gptr.Indirect(RawCall).setRetryPolicy(rp)
 }
