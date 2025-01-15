7612381b82d11acbfcfa5d08d845e4927efe3f7a
Wang Xinglong <wangxinglong.123@bytedance.com>
2025-01-16 00:11:23 +0800

    update

--- reader/reader.go
+++ reader/reader.go
@@ -5,14 +5,14 @@ import (
 	"github.com/spf13/viper"
 )
 
-func InitAppConfig[T struct{}]() *T {
+func InitAppConfig[T any]() *T {
 	vip := viper.New()
 	vip.SetConfigFile("conf/" + env.GetEnv() + ".yaml")
 	err := vip.ReadInConfig()
 	if err != nil {
 		panic(err)
 	}
-	Config := &T{}
+	Config := new(T)
 	err = vip.Unmarshal(Config)
 	if err != nil {
 		panic(err)
