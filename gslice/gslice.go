026bad4822a040c3ae8bfd9612fa79d542dbb838
Wang Xinglong <wangxinglong.123@bytedance.com>
2025-01-16 19:37:58 +0800

    update

--- gslice/gslice.go
+++ gslice/gslice.go
@@ -13,3 +13,12 @@ func Map[T, U any](s []T, f func(T) U) (r []U) {
 	}
 	return
 }
+
+func Filter[T any](s []T, f func(T) bool) (r []T) {
+	for _, v := range s {
+		if f(v) {
+			r = append(r, v)
+		}
+	}
+	return
+}
