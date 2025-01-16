45384de212b897db1e3df98253a5b8d3f650ca68
Wang Xinglong <wangxinglong.123@bytedance.com>
2025-01-16 22:49:30 +0800

    update

---
+++ Status/status.go
@@ -0,0 +1,23 @@
+package Status
+
+import "github.com/qcq1/common/gptr"
+
+type Status struct {
+	code    *int64
+	message *string
+}
+
+func (s *Status) Code() *int64 {
+	return s.code
+}
+
+func (s *Status) Message() *string {
+	return s.message
+}
+
+var (
+	Success = &Status{
+		code:    gptr.Of(int64(0)),
+		message: gptr.Of("success"),
+	}
+)
