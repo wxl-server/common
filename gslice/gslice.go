b8c8589e8ad3932472b06083b72f00d07c589656
Wang Xinglong <wangxinglong.123@bytedance.com>
2025-01-24 17:13:28 +0800

    update

--- gslice/gslice.go
+++ gslice/gslice.go
@@ -22,3 +22,21 @@ func Filter[T any](s []T, f func(T) bool) (r []T) {
 	}
 	return
 }
+
+func ToMap[T any, K comparable, V any](s []T, f func(T) (K, V)) (r map[K]V) {
+	r = make(map[K]V)
+	for _, v := range s {
+		k, v := f(v)
+		r[k] = v
+	}
+	return
+}
+
+func GroupBy[T any, K comparable](s []T, f func(T) K) (r map[K][]T) {
+	r = make(map[K][]T)
+	for _, v := range s {
+		k := f(v)
+		r[k] = append(r[k], v)
+	}
+	return
+}
